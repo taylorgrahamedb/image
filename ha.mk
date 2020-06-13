d5127b0a731d4e25d345f6a151d76a8ac92bb6ab
thecloudslyer <taylor.c.graham@gmail.com>
2020-06-12 23:14:04 -0400

    first

--- ha.mk
+++ ha.mk
@@ -13,9 +13,9 @@ export $(shell sed 's/=.*//' $(cnf))
 # HELP
 # This will output the help for each task
 # thanks to https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
-.PHONY: help build_container push-container
+.PHONY: help build_container push-contain ser
 
-help: ## This help.
+help: ## This help me s.
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 .DEFAULT_GOAL := help
